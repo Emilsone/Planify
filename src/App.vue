6bb8d7093b2d9c846e9a1f9400d055d2ef85235d
Emilsone <lawsonezekiel68@gmail.com>
2020-11-16 12:51:58 +0100

    create vue components

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Message />
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Message from './components/Message.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Message
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <div class="hero___section">
+      <div class="hero-text">
+        <h1>Figma ecommerce dashboard template</h1>
+        <p>
+          Make sure to have a look at this cool new Figma resource shared by
+          Bartosz Maryniaczyk. A free e-commerce dashboard template that can be
+          readily used for your own project needing a dashboard.
+        </p>
+      </div>
+      <div>
+        <img alt="Vue logo" src="../assets/hero.png" />
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "Message",
+};
+</script>
+<style>
+.hero___section {
+  display: flex;
+  justify-content: space-around;
+}
+.hero-text{
+  font-size: 20px;
+  text-align: left
+}
+.hero-text h1{
+font-size: 30px;
+}
+</style>
